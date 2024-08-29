9c0f2228c17fcb991328cef6f542538c9e4775f0
stivengm <stivenalexander7u7@gmail.com>
2024-08-28 22:33:47 -0500

    FIX - Ajuste de formato de hora.

--- src/index.ts
+++ src/index.ts
@@ -143,7 +143,7 @@ export const formattedTime = (timeStamp: any) => {
     const date = new Date(timeStamp.seconds * 1000);
 
     let hours = date.getHours();
-    let minutes = date.getHours();
+    let minutes = date.getMinutes();
 
     const ampm = hours >= 12 ? "PM" : "AM";
     const formattedHours = hours % 12 === 0 ? "12" : (hours % 12).toString().padStart(2, "0");
