b3b89146e7debd29622cabf9c370347950a2bfa1
stivengm <stivenalexander7u7@gmail.com>
2024-11-14 23:14:44 -0500

    FIX - Ajuste para devolver la fecha y hora de méxico.

--- src/index.ts
+++ src/index.ts
@@ -223,13 +223,16 @@ export const getMonthYearToString = (date: string) => {
 export const getCurrentDate = () => {
     const options: Intl.DateTimeFormatOptions = {
         timeZone: 'America/Mexico_City',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit',
-        hour12: false,
+        hour12: false
     };
 
     const date = new Date();
-
+    
     return new Intl.DateTimeFormat('en-US', options).format(date);
 }
